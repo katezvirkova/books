a9c885d7a40da1ac481efa7246e68e3f4df4b05f
Звіркова <katezvirkova@gmail.com>
2024-10-13 14:16:49 +0300

    Update URLs, admin, models, serializers, and views

--- myapi/models.py
+++ myapi/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Bookmark(models.Model):
+    url = models.URLField(max_length=500)
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    favorite = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
