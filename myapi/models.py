a58ceb678c9ae9df8390a05362dd901c76820670
katezvirkova <167428792+katezvirkova@users.noreply.github.com>
2024-10-15 19:55:17 +0300

    Update models.py

--- myapi/models.py
+++ myapi/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User  # Імпортуємо модель User
+from django.contrib.auth.models import User 
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
