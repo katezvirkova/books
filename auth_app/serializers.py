2e6a9453a479f75f4eac481b0463ecbe1cf646ca
Звіркова <katezvirkova@gmail.com>
2024-10-15 20:23:44 +0300

    update files

--- auth_app/serializers.py
+++ auth_app/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+import re
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, min_length=8)
@@ -10,8 +11,20 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ['username', 'first_name', 'last_name', 'email', 'password', 'password_check']
 
     def validate(self, data):
+        required_fields = ['username', 'first_name', 'last_name', 'email', 'password']
+        for field in required_fields:
+            if not data.get(field):
+                raise serializers.ValidationError({field: f"{field.capitalize()} is required."})
+
         if data['password'] != data['password_check']:
             raise serializers.ValidationError("Passwords do not match.")
+
+        if len(data['password']) < 8:
+            raise serializers.ValidationError("Password must be at least 8 characters long.")
+
+        if not re.search(r'\d', data['password']) or not re.search(r'[A-Za-z]', data['password']):
+            raise serializers.ValidationError("Password must contain at least one letter and one number.")
+
         return data
 
     def create(self, validated_data):
